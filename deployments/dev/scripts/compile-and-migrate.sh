d055f33f58c85fba3e141941777a4abde5b451c9
0x96dBF <0x96dBF@protonmail.com>
2020-03-09 15:43:43 +0000

    Add launch scripts for dev environment

---
+++ deployments/dev/scripts/compile-and-migrate.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+npm install &&
+    truffle compile --contracts_directory ./migration_contract --contracts_build_directory /build --network development &&
+    truffle migrate --contracts_build_directory /build --network development
