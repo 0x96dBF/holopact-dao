20866a5924b9091cd7c1b0b60915837441c6d7f5
0x96dBF <0x96dBF@protonmail.com>
2020-03-09 13:00:44 +0000

    Add basic ERC777 token implementation

---
+++ src/contracts/token/HolopactToken.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+import "@openzeppelin-solidity/contracts/token/ERC777/ERC777.sol"
+
+
+/// @title Basic ERC777 token
+contract HolopactToken is ERC777 {
+  
+  constructor(uint256 initialSupply,
+              address[] memory defaultOperators
+              )
+    ERC777("Holopact", "HOLO", defaultOperators)
+    public {
+    _mint(msg.sender, msg.sender, initialSupply, "", "");        
+  }
+  
+}
