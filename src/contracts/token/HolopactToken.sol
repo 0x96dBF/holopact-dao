d643a7851c32fd65208ca9f0135d2d513afcc13d
0x96dBF <0x96dBF@protonmail.com>
2020-03-10 23:37:18 +0000

    Fix formatting as per solium

--- src/contracts/token/HolopactToken.sol
+++ src/contracts/token/HolopactToken.sol
@@ -5,9 +5,13 @@ import "./DividendToken.sol";
 
 /// @title Basic ERC777 token
 contract HolopactToken is DividendToken {
-  
-  constructor(uint256 initialSupply)
-    DividendToken("Holopact", "HOLO", new address[](0), initialSupply * (10 ** 18), 1)
+
+    constructor(uint256 initialSupply)
+        DividendToken("Holopact",
+                      "HOLO",
+                      new address[](0),
+                      initialSupply * (10 ** 18),
+                      1)
     public {}
 
 }
